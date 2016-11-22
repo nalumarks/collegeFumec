32875b8ad0f1d42488ae51be491a030e148cbe16
Anna Luiza Marques <naninhamarks@gmail.com>
2016-11-22 19:57:00 -0200

    Update cadAluno.c

--- cadAluno.c
+++ cadAluno.c
@@ -3,7 +3,7 @@
 int main(){
 	int tamanho;
 	
-	prinff("Informe a quantidade de alunos a ser cadastrados:\n")
+	printf("Informe a quantidade de alunos a ser cadastrados:\n");
 	scanf("%d", &tamanho);
 	
 	system("cls");
