180f7c46c09963127870828ed45febd6937e06e7
Anna Luiza Marques <naninhamarks@gmail.com>
2016-11-22 20:33:38 -0200

    Update cadAluno.c

--- cadAluno.c
+++ cadAluno.c
@@ -12,17 +12,44 @@ int main(){
 }
 
 void menuPrincipal(int tam){
-	Usuario* user;
+	Usuario* p_User;
 	int op; //op = opção
 	do{
 		system("cls");
-		
-		printf("+---------------------------------------------+");
-		printf("\n|              MENU                         |");
+		printf("\n+--------------INFORME UMA OPCAO--------------+");
 		printf("\n+---------------------------------------------+");
-		printf("\n|  [1] - CADASTRAR USUARIO                    |");
+		printf("\n|  (1) - CADASTRAR USUARIO                     |");
+		printf("\n|  (0) - SAIR                                 |");
+		printf("\n+---------------------------------------------+");
 		
+		printf("\n\nESCOLHA UMA OPCAO: ");
 		scanf("%d", &op);
 		
+		switch(op){
+			case 0 : break;
+			case 1 : { 
+				p_User = cadastroAluno(tam);
+				break;
+			}//fim case 1
+			default:{
+				printf("Opcao invalida! Por favor, informe outra opcao :) ");
+				break;
+			}
+			
+		}//fim switch
+		
 	}while (op !=0);
+	
+}//fim menu principal
+
+Usuario *cadastroAluno(int tam){
+	Usuario *user;
+	Usuario *auxiliar;
+	
+	
+	user = (Usuario*) malloc(tam*sizeof(Usuario));
+	if(user=NULL){
+		printf("Alocacao nao realizada");
+	}
+	
 }
