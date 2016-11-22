16b40fa893da3060af1048d1fc5c486af3bcd782
Anna Luiza Marques <naninhamarks@gmail.com>
2016-11-22 20:10:27 -0200

    Update cadAluno.c

--- cadAluno.c
+++ cadAluno.c
@@ -1,12 +1,28 @@
 #include "cadAluno.h"
-
 int main(){
 	int tamanho;
 	
 	printf("Informe a quantidade de alunos a ser cadastrados:\n");
 	scanf("%d", &tamanho);
 	
+	menuPrincipal(tamanho);
 	system("cls");
 	
 	return 0;
+}
+
+void menuPrincipal(int tam){
+	Usuario* user;
+	int op; //op = opção
+	do{
+		system("cls");
+		
+		printf("+---------------------------------------------+");
+		printf("\n|              MENU                         |");
+		printf("\n+---------------------------------------------+");
+		printf("\n|  [1] - CADASTRAR USUARIO                    |");
+		
+		scanf("%d", &op);
+		
+	}while (op !=0);
 }
