7928c8be9f860adbafcbb9d3cd679a6f784b3636
Anna Luiza Marques <naninhamarks@gmail.com>
2016-11-23 22:30:21 -0200

    Update myString.h

--- myString.h
+++ myString.h
@@ -1,31 +1,28 @@
-//#endif
-
 #ifndef new
 	#define new {.length=length, .equals=equals, .equalsIgnoreCase=equalsIgnoreCase, .substring=substring,.toUpperCase=toUpperCase, .toLowerCase=toLowerCase, .replace=replace, .lastIndexOf=lastIndexOf, .firstIndexOf=firstIndexOf};
 #endif
 
 #include "myString.c"
-
+// #ifndef STRING
+//	#define STRING
+	
 int length(char *str);
 int firstIndexOf(char *str, char str_at);
 int lastIndexOf(char *str, char str_at);
 int equals(char *str, char *str_cmp);
-char *toUpperCase(char *str);
-char *toLowerCase(char *str);
+void toUpperCase(char *str);
+void toLowerCase(char *str);
 char *replace(char *str, char str_old, char str_new);
 char *substring(char *str, int primeiro, int ultimo);
-int equalsIgnoreCase(char *str, char *str_cmp) ;
-
-//#ifndef STRING
-//	#define STRING
+int equalsIgnoreCase(char *str, char *str_cmp);
 
 	typedef struct {
 		int   (*length)(char*);
 		int   (*equals)(char*, char*);
 		int   (*equalsIgnoreCase)(char*, char*);
 		char* (*substring)(char*, int, int);
-		char* (*toUpperCase)(char*);
-		char* (*toLowerCase)(char*);
+		void  (*toUpperCase)(char*);
+		void  (*toLowerCase)(char*);
 		char* (*replace)(char*, char, char);
 		int   (*lastIndexOf)(char*, char);
 		int   (*firstIndexOf)(char*, char);
