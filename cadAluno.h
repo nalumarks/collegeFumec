41f47e083d9f1395f708a286db62fe555e2b8792
Anna Luiza Marques <naninhamarks@gmail.com>
2016-11-29 21:58:39 -0200

    Update cadAluno.h

--- cadAluno.h
+++ cadAluno.h
@@ -27,7 +27,7 @@ void pesquisarSobreNome (Usuario *user, int tam);
 char gerarCod(Usuario *user, int tam);
 void pesquisarAnoNasc (Usuario *user, int tam);
 void pesquisarMesNasc (Usuario *user, int tam);
-
+void pesquisarMatricula (Usuario *user, int tam);
 #ifndef WINDOWS
 	#define WINDOWS
 #endif
