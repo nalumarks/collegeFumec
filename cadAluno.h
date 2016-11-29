c65ad385cc81cd3f2866481ec6201908136f48ed
Anna Luiza Marques <naninhamarks@gmail.com>
2016-11-29 17:45:01 -0200

    Update cadAluno.h

--- cadAluno.h
+++ cadAluno.h
@@ -8,6 +8,8 @@ typedef struct {
 	char nome [200];
   	char endereco [200];
   	char dtNasc [11];
+  	int sequencial;
+  	char matricula[8];
 }Usuario;
 
 typedef struct {
@@ -22,6 +24,7 @@ Usuario *cadastroAluno(int tam);
 int camposVazios(char *str);
 int validarData (char *data);
 void pesquisarSobreNome (Usuario *user, int tam);
+char gerarCod(Usuario *user, int tam);
 //preencher depois que a pesquisa estiver pronta
 void buscarNumeroCasa();
 void buscarSobrenome();
