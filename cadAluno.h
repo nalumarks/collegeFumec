2b05f93c5576aa4269ab1905dcbd9bf65043c5c3
Anna Luiza Marques <naninhamarks@gmail.com>
2016-11-30 22:21:36 -0200

    Update cadAluno.h

--- cadAluno.h
+++ cadAluno.h
@@ -21,13 +21,14 @@ typedef struct {
 void menuPrincipal(int tam);
 
 Usuario *cadastroAluno(int tam);
-int camposVazios(char *str);
+int camposVazios(char *campos);
 int validarData (char *data);
 void pesquisarSobreNome (Usuario *user, int tam);
 char gerarCod(Usuario *user, int tam);
 void pesquisarAnoNasc (Usuario *user, int tam);
 void pesquisarMesNasc (Usuario *user, int tam);
 void pesquisarMatricula (Usuario *user, int tam);
+void listarUsuario(Usuario *user, int tam);
 #ifndef WINDOWS
 	#define WINDOWS
 #endif
